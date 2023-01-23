32f8cbb3143ff3a24dc7185fc2af34b6c74ef811
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-01-23 21:54:23 +0800

    formatted

--- javascriptlogin.js
+++ javascriptlogin.js
@@ -1,33 +1,28 @@
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
-function validate(){
-    var username = document.getElementById("Username").value;
-    var password = document.getElementById("Password").value;
-if ( username == "admin" && password == "admin123"){
-    alert ("Login successfully");
+function validate() {
+  var username = document.getElementById("Username").value;
+  var password = document.getElementById("Password").value;
+  if (username == "admin" && password == "admin123") {
+    alert("Login successfully");
     return false;
-}
-else if(username == "raven" && password =="raven123"){
-    alert ("Login successfully")
+  } else if (username == "raven" && password == "raven123") {
+    alert("Login successfully");
     return false;
-}
-else if(username == "armil" && password =="armil123"){
-    alert ("Login successfully")
+  } else if (username == "armil" && password == "armil123") {
+    alert("Login successfully");
     return false;
-}
-else if(username == "ydrian" && password =="ydrian123"){
-    alert ("Login successfully")
+  } else if (username == "ydrian" && password == "ydrian123") {
+    alert("Login successfully");
     return false;
-}
-else{
-alert("Failed! Please Try Again!");
-// Disabling fields after 3 attempts.
-    if( attempt == 0){
-    document.getElementById("username").disabled = true;
-    document.getElementById("password").disabled = true;
-    document.getElementById("submit").disabled = true;
-    return false;
+  } else {
+    alert("Failed! Please Try Again!");
+    // Disabling fields after 3 attempts.
+    if (attempt == 0) {
+      document.getElementById("username").disabled = true;
+      document.getElementById("password").disabled = true;
+      document.getElementById("submit").disabled = true;
+      return false;
     }
+  }
 }
-}
-
